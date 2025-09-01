75233ef1dba093db5b18e66496d624f626d815f1
Ashf5 <friedash85@gmail.com>
2025-09-01 08:49:04 +0300

    add string

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -4,16 +4,8 @@ import knex from "knex";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const {PGUSER, PGPASSWORD, PGHOST, PGDATABASE} = process.env;
-let Port = 5432;
-
 export const db = knex({
     client: 'pg',
-    connection: {
-        host: PGHOST,
-        user: PGUSER,
-        password: PGPASSWORD,
-        database: PGDATABASE,
-        port: Port
-    }
+    connection: process.env.PGDATABASE,
+    pool: { min: 0, max: 10 },
 });
